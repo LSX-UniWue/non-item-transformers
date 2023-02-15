da0acef909ee36ab87a28af1109cf386805b267f
Elisabeth Fischer <elisabeth.fischer@informatik.uni-wuerzburg.de>
2023-02-15 11:14:18 +0100

    cleanup

--- src/asme/core/tokenization/special_values.py
+++ src/asme/core/tokenization/special_values.py
@@ -1,36 +1,4 @@
-from random import random
-from typing import Any
-
 from asme.data.utils.converter_utils import build_converter
-
-
-def build_random(feature_type: str
-                    ) -> Any:
-    if feature_type == 'int':
-        return int
-
-    if feature_type == 'float':
-        return float
-
-    if feature_type == 'bool':
-        return bool(random.randrange(0, 1))
-
-    if feature_type == 'timestamp':
-        return functools.partial(_parse_timestamp, date_format=configs.get('format'))
-
-    if feature_type == 'list':
-        element_type = configs.get("element_type")
-        delimiter = configs.get('delimiter')
-        converter = build_converter(element_type, configs)
-        return functools.partial(_parse_list, delimiter=delimiter, converter=converter)
-
-    if feature_type == 'pd.array':
-        element_type = configs.get("element_type")
-        converter = build_converter(element_type, configs)
-        return functools.partial(_parse_pd_array, converter=converter)
-
-    raise KeyError(f'{feature_type} not supported. Currently only bool, timestamp and int are supported. '
-                   f'See documentation for more details')
 
 class SpecialValues:
     """
